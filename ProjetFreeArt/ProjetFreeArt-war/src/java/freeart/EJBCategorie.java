2464b1843f4c56f86ace7013ffd22303ff8a37e6
Agatio <sebalandre@gmail.com>
2015-01-16 14:44:37 +0100

    Ajout des EJB Session et des Servlets

---
+++ ProjetFreeArt/ProjetFreeArt-war/src/java/freeart/EJBCategorie.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package freeart;
+
+import javax.ejb.Stateful;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author Agatio
+ */
+@Stateful
+@LocalBean
+public class EJBCategorie {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+}
